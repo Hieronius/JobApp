6144b0003a7a2863239c0c4fa2ee6de5398def49
Arsenty Halimovsky <ghost12@yandex.ru>
2024-03-15 04:24:16 +0300

    Create a new project

---
+++ JobApp/JobApp/JobAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  JobAppApp.swift
+//  JobApp
+//
+//  Created by Арсентий Халимовский on 15.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct JobAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
